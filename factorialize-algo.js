1108ddf94166175066f8ec932938c6fe0f5da77d
Chad Lengefeld <36778876+LostnAustin@users.noreply.github.com>
2022-11-03 05:01:17 -0500

    Create factorialize-algo.js

---
+++ factorialize-algo.js
@@ -0,0 +1,10 @@
+//Factorial: product of all integers > 0 <= num
+
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+factorialize(5);
